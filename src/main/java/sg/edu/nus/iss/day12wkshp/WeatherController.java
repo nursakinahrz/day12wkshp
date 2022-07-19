387ab9a38483b4b2168698cccf671c38cf6bf65c
Nursakinah <nursakinah.arz@gmail.com>
2022-07-19 14:12:48 +0800

    WeatherController modified

--- src/main/java/sg/edu/nus/iss/day12wkshp/WeatherController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/WeatherController.java
@@ -23,6 +23,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
                 return "weather";
 
             }
+
+            @GetMapping("{city}")
+            public String weather2(
+                @PathVariable(name="city", required = true) String city,
+                @RequestParam(name = "units", defaultValue="kilometers") String units,
+                Model model) {
+
+                model.addAttribute("city", city);
+                model.addAttribute("units", units);
+
+                return "weather";
+                }
+            
         
     }
 
