1d0134f98477acb6b7a707ff48a01eaed6bbdc96
Nursakinah <nursakinah.arz@gmail.com>
2022-07-19 09:56:13 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/Day12wkshpApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day12wkshp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12wkshpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12wkshpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day12wkshp/Day12wkshpApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day12wkshp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12wkshpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
