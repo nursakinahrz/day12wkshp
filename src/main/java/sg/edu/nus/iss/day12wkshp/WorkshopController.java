bdee37d5345d5f7d4b3cdc1f7591366e8768a7a9
Nursakinah <nursakinah.arz@gmail.com>
2022-07-19 16:26:44 +0800

    workshop 2 completed

--- src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/WorkshopController.java
@@ -4,15 +4,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import java.util.*;
-import java.lang.*;
+import java.lang.Integer;
 
 @Controller
 public class WorkshopController {
     
     @RequestMapping(path= { "/workshop"})
     public String test() {
-        return "workshop";
-
-        
+        return "workshop"; 
     }
+
+    @RequestMapping(path= { "/processNumber"})
+    public String testResults(
+        @RequestParam(name="inputNumber", defaultValue = "1") Integer unit,
+        Model model) {
+
+            List<Integer> ListInt = new ArrayList<>();
+            int loopValue = Integer.parseInt(unit.toString());
+
+            int loop = 1;
+            while (loop <= loopValue) {
+
+                int result = (int) (Math.random() * (loopValue) + 1);
+
+                // to check if the number not in the list, then add it to the loop and increase it
+                if (!ListInt.contains(Integer.valueOf(result))) {
+                    ListInt.add(Integer.valueOf(result));
+                    loop++;
+                }
+            }
+
+            for (int i = 0; i <ListInt.size(); i++) {
+                System.out.println("==> " + ListInt.get(i));
+                System.out.println();
+            }
+            model.addAttribute("numbers", ListInt);
+            
+            return "workshopresult";
+
+        }
 }
