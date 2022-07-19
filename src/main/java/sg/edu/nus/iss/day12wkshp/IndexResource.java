a47dd5f89e4c13e3893e5d5330adb3f8af2ba181
Nursakinah <nursakinah.arz@gmail.com>
2022-07-19 11:32:33 +0800

    added time and calendar

--- src/main/java/sg/edu/nus/iss/day12wkshp/IndexResource.java
+++ src/main/java/sg/edu/nus/iss/day12wkshp/IndexResource.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.day12wkshp;
 
+
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +17,14 @@ public class IndexResource {
     @GetMapping(produces = {"text/html"})
     //model is required for dynamic content
     public String index(Model model) {
+        model.addAttribute("currTime", (new Date()).toString());
+        
+        Calendar cal = Calendar.getInstance();
+        model.addAttribute("currHour", cal.get(Calendar.HOUR_OF_DAY));
+        
+        
         return "indexresource";
+        
 
 
     }
